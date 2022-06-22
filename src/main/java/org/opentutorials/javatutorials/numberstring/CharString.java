596387e21ecbd5937156b47b766df95b82535b9a
soyeonkim0227 <soyeonkim0227@naver.com>
2022-06-23 08:35:01 +0900

    Create numberString

---
+++ src/main/java/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+    public static void main(String[] args) {
+        // 문자 Character
+        System.out.println('a');
+
+        // 문자열 String
+        System.out.println("생");
+    }
+}
---
+++ src/main/java/org/opentutorials/javatutorials/numberstring/Number.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+
+    public static void main(String[] args) {
+        // 더하기
+        System.out.println(2+5);
+
+        // 곱하기
+        System.out.println(6*3);
+
+        // 나누기
+        System.out.println(8/4);
+    }
+}
