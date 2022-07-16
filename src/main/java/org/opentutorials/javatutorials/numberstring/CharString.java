9a3b8b59708c46f78d144d4498260f43e778ad6e
soyeonkim0227 <soyeonkim0227@naver.com>
2022-07-16 23:44:46 +0900

    Update numberString

--- src/main/java/org/opentutorials/javatutorials/numberstring/CharString.java
+++ src/main/java/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -6,6 +6,15 @@ public class CharString {
         System.out.println('a');
 
         // 문자열 String
-        System.out.println("생");
+        System.out.println("김소연"+"입니다");
+
+        // '+' 이용
+        System.out.println("1"+"1");
+
+        // 큰따옴표 표시
+        System.out.println("내가 말했다!\"안녕하세요저는김소연입니당ㅋㅋ\"");
+
+        // 줄바꿈
+        System.out.println("ㅎㅇ\n\"ㅎㅇ\"");
     }
 }
