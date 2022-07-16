84ecffc7d42680220d1b788a7b5e7de84f7066b5
soyeonkim0227 <soyeonkim0227@naver.com>
2022-07-16 23:45:11 +0900

    Creat variables

---
+++ src/main/java/org/opentutorials/javatutorials/variables/DoubleDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.variables;
+
+public class DoubleDemo {
+
+    public static void main(String[] args) {
+        double a; // double은 실수
+        a = 1.1;
+
+        System.out.println(a+1.1);
+    }
+}
---
+++ src/main/java/org/opentutorials/javatutorials/variables/IntegerDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntegerDemo {
+
+    public static void main(String[] args) {
+        int a;
+        a = 1; // int는 데이터 형식(정수), a는 변수의 이름 ((변수 선언))
+        System.out.println(a+1); // 2
+
+        a = 2;
+        System.out.println(a+1); // 3
+    }
+}
